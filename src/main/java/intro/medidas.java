fc8526e51babb2b385180bb7a721d610bdcc752b
biamartini <bianca.martini.evs@gmail.com>
2022-03-16 14:34:29 -0300

    mudanca do calculo

--- src/main/java/intro/medidas.java
+++ src/main/java/intro/medidas.java
@@ -2,34 +2,41 @@ package intro;
 
 public class medidas {
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
-        System.out.println("Cálculo de área");
+        calcularAreaModoCurto();
+        calcularAreaModoExtenso();
+    }
 
+        public static void calcularAreaModoCurto(){
+
+            System.out.println("Cálculo de área Modo curto");
+
+            int largura = 4;
+            int comprimento = 3;
+
+
+            System.out.println("Para a largura de " + largura + " m2 e o comprimento de " + comprimento +
+                    " m2, a area é de " + largura * comprimento +"m2");
 
         // calculo reduzido
 
         }
-        public void calcularAreaModoExtenso(){
-            int largura;
-            int comprimento;
-            int resultado;
-
-            largura = 5;
-            comprimento = 6;
-
-            resultado = largura * comprimento;
-            System.out.println("Para a largura de " + largura + " m2 e o comprimento de " + comprimento +
-                    " m2, a area é de " + resultado + "  m2");
-
-        }
-        public void calcularAreaModoCompacto(){
-        int largura = 4;
-        int comprimento = 3;
 
 
-            System.out.println("Para a largura de " + largura + " m2 e o comprimento de " + comprimento +
-                    " m2, a area é de " + resultado + "  m2");
+            public static void calcularAreaModoExtenso(){
 
+                System.out.println("Calculo de area modulo extenso");
+                int largura;
+                int comprimento;
+                int resultado;
+
+                largura = 5;
+                comprimento = 6;
+
+                resultado = largura * comprimento;
+
+                System.out.println("Para a largura de " + largura + " m2 e o comprimento de " + comprimento +
+                        " m2, a area é de " + resultado + "  m2");
     }
 }
