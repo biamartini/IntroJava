04a743e1df73612e9f220fa04035a78049637b79
biamartini <bianca.martini.evs@gmail.com>
2022-03-16 14:18:36 -0300

    Initial commit

---
+++ src/main/java/intro/medidas.java
@@ -0,0 +1,35 @@
+package intro;
+
+public class medidas {
+
+    public static void main (String[] args){
+
+        System.out.println("Cálculo de área");
+
+
+        // calculo reduzido
+
+        }
+        public void calcularAreaModoExtenso(){
+            int largura;
+            int comprimento;
+            int resultado;
+
+            largura = 5;
+            comprimento = 6;
+
+            resultado = largura * comprimento;
+            System.out.println("Para a largura de " + largura + " m2 e o comprimento de " + comprimento +
+                    " m2, a area é de " + resultado + "  m2");
+
+        }
+        public void calcularAreaModoCompacto(){
+        int largura = 4;
+        int comprimento = 3;
+
+
+            System.out.println("Para a largura de " + largura + " m2 e o comprimento de " + comprimento +
+                    " m2, a area é de " + resultado + "  m2");
+
+    }
+}
